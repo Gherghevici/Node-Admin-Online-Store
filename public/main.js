000d858d0a2ae4c4d9c0996d6cc54da88cc87ebf
Kapacioc <andreigherghevici@gmail.com>
2023-08-16 00:46:51 +0300

    16.08.2023

---
+++ public/main.js
@@ -0,0 +1,9 @@
+const displayData = document.getElementById("displayData");
+const modalSection = document.getElementById("modalSection");
+const addNewProduct = document.getElementById("addNewProduct");
+
+addNewProduct.addEventListener("click",(e)=>{
+    modalSection.classList.toggle("hidden")
+})
+
+
