363d14724ffe2481ddab4971789fec1d95869a7a
Andrei Gherghevici <andreigherghevici@gmail.com>
2023-08-17 01:24:37 +0300

    17.08.2023

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
+const data = require("./data.js");
+
 app.use('/',express.static('./public'))
+app.use(bodyParser.json());
+
+app.post("/data",(req,res)=>{
+    let body = req.body;
+    const newData = {
+        id:data.length+1,
+        title:body.title,
+        description:body.description,
+        price:body.price,
+        quantity:body.quantity
+    }
+    data.push(newData);
+    res.json(data);
+})
+app.get("/data",(req,res)=>{
+    res.send(data)
+})
+
 
 app.get('/',(req,res)=>{
     res.send('hello')
--- public/main.js
+++ public/main.js
@@ -1,9 +1,78 @@
+//sections 
 const displayData = document.getElementById("displayData");
 const modalSection = document.getElementById("modalSection");
-const addNewProduct = document.getElementById("addNewProduct");
 
+//inputs
+const title = document.getElementById("title");
+const description = document.getElementById("description");
+const price = document.getElementById("price");
+const quantity = document.getElementById("quantity");
+
+//buttons
+const addNewProduct = document.getElementById("addNewProduct");
+const sendInfoBtn = document.getElementById("sendInfoBtn");
+
+//get or post data
+const getData = async()=>{
+   const res = await fetch('http://localhost:3000/data')
+    const data = await res.json();
+    return data
+}
+
+
+
+
+const sendData= ()=>{
+    const newData={
+        title:title.value,
+        description:description.value,
+        price:price.value,
+        quantity:quantity.value
+    }
+    fetch("http://localhost:3000/data",{
+        method: "POST",
+        body: JSON.stringify(newData),
+        headers:{
+            "Content-type": "application/json; charset=UTF-8"
+        }
+    })
+}
+
+
+//data handeling
+const creatingDataUI = async()=>{
+    const line =document.createElement("div");
+    line.classList.add("line")
+    const t = document.createElement("div");
+    const d = document.createElement("div");
+    const p = document.createElement("div");
+    const q = document.createElement("div");
+    
+    const data = await getData()
+    if(data){
+        data.forEach(value => {
+            t.innerText=value.title;
+            d.innerText=value.description;
+            p.innerText=value.price;
+            q.innerText=value.quantity;
+            line.append(t,d,p,q)
+            displayData.appendChild(line);
+        });
+        
+        
+    
+    }
+    
+}
+
+
+//event listeners
 addNewProduct.addEventListener("click",(e)=>{
     modalSection.classList.toggle("hidden")
 })
 
-
+sendInfoBtn.addEventListener("click",(e)=>{
+    e.preventDefault()
+    sendData();
+    creatingDataUI();
+})
